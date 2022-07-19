00b60a0e9cbfc4d1ad60cb07335b21f133c7deb7
Eliocre <71342502+Eliocre@users.noreply.github.com>
2022-07-19 12:47:15 +0200

    lezgo transfer

--- 18ans/script/Player.gd
+++ 18ans/script/Player.gd
@@ -6,17 +6,19 @@ const MAX_SPEED = 60
 
 var velocity = Vector2.ZERO
 
-
+#Animations
 onready var animationPlayerAzeroth = $Azeroth/AnimationAzeroth
 onready var animationAzerothTree = $Azeroth/AnimationTree
 onready var animationPlayerPeb = $Peb/AnimationPeb
 onready var animationPebTree = $Peb/AnimationTree
+
 onready var animationPebstate = animationPebTree.get("parameters/playback")
 
 
 
 func _physics_process(delta):
 	var input_vector = Vector2.ZERO
+	
 	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
 	input_vector = input_vector.normalized()
